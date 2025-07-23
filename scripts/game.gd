b6c2e356402a8a049c0a1288a2dae00b4d3a7dea
Nikko Purba <nikko.purba@gmail.com>
2025-07-24 00:05:52 +0200

    Refactored positioning of spawn/queupoints, made john actually spawn

--- scripts/game.gd
+++ scripts/game.gd
@@ -3,10 +3,14 @@ extends Node2D
 @onready var stage = $Stage
 @onready var spawnTimer: Timer = $SpawnTimer
 
+var johnScene = load("res://scenes/john.tscn")
+
 var queue: Array
+var johnSpawnPoint: Vector2
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	initSpawnPoint()
 	initQueuePositions()
 	startRound()
 	print(queue)
@@ -16,6 +20,10 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+
+func initSpawnPoint() -> void:
+	var spawnPoint: Node2D = stage.get_node("JohnSpawnPoint")
+	self.johnSpawnPoint = spawnPoint.transform.get_origin()
 
 func initQueuePositions() -> void:
 	var queuePositionsContainer = stage.get_node("QueuePositions")
@@ -27,6 +35,9 @@ func initQueuePositions() -> void:
 func startRound() -> void:
 	startSpawnTimer()
 
+func restartSpawnTimer() -> void:
+	spawnTimer.wait_time = randi_range(10, 20)
+	spawnTimer.start()
 
 func startSpawnTimer() -> void:
 	spawnTimer.wait_time = randi_range(10, 20)
@@ -34,5 +45,20 @@ func startSpawnTimer() -> void:
 	spawnTimer.start()
 
 func spawnJohn() -> void:
+	var johnInstance: John = johnScene.instantiate()
+	johnInstance.stats = generateRandomStats()
+	johnInstance.transform.origin = johnSpawnPoint
+	add_child(johnInstance)
 	print("A john has spawned")
-	startSpawnTimer()
+	restartSpawnTimer()
+
+
+func generateRandomStats() -> Stats:
+	var stats = Stats.new()
+	stats.cuteToHot = randf_range(-1,1)
+	stats.subToDom = randf_range(-1,1)
+	stats.talk = randi_range(0,100)
+	stats.romance = randi_range(0,100)
+	stats.party = randi_range(0,100)
+	stats.sports = randi_range(0,100)
+	return stats
