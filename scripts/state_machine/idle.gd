7ef8ce557dd213f818967604e185e9f15b513712
Nikko <nikko.purba@gmail.com>
2025-08-30 01:47:15 +0200

    removed state selected (is currently deemed other system), Made idle state handle event.

--- scripts/state_machine/idle.gd
+++ scripts/state_machine/idle.gd
@@ -7,3 +7,8 @@ func mouse_over() -> void:
 func mouse_exit() -> void:
 	print("hoe mouse exit")
 	character.statsWindowSpawner.hideStatsWindow()
+
+
+func process_input(event: InputEvent) -> State:
+	character.selectable.select(event,character)
+	return null
